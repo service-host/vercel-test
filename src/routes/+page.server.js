152b591b7ee19f55168add1d0f58e1e3b8548d49
@kaspar <team@crewnew.com>
2023-01-11 13:30:31 +0000

    Try again to remove the Hasura auth

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -29,8 +29,7 @@ export const actions = {
 			method: 'POST',
 			body: body,
 			headers: {
-				'Content-type': 'application/json; charset=UTF-8',
-				'x-hasura-admin-secret': env.HASURA_ADMIN_SECRET
+				'Content-type': 'application/json; charset=UTF-8'
 			}
 		})
 			.then((res) => {
